64da98c8ee60a0c0a0f421997350aff48aac1b00
bernard4132 <bernardlomo2@gmail.com>
2019-05-08 13:50:14 +0000

    new push

--- app/models/fabric.rb
+++ app/models/fabric.rb
@@ -2,4 +2,10 @@ class Fabric < ApplicationRecord
 	mount_uploader :fabricimage, FabricimageUploader
 	has_many :requests
 	default_scope -> { order('created_at DESC') }
+	before_create :generate_fabricordercode
+
+	def generate_fabricordercode
+       self.price = "NFD" + rand(101..999).to_s + "-" + "NFD" + rand(10011..99099).to_s 
+  end
+
 end
